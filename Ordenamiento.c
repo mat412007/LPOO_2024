a474a034ae80717e80dd25511e3b78e68ffb6d90
Appaz <matias2007play4@gmail.com>
2024-04-29 17:11:39 -0300

    Update

---
+++ Ordenamiento.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(int argc, char *arv[]){
+    int max = 1000;
+    int aux
+}
--- PPP.c
+++ PPP.c
@@ -7,6 +7,7 @@ int main(int argc, char *argv[]){
     int ronda = 1;
     int puntaje_jugador = 0;
     int puntaje_maquina = 0;
+    char *movimientos[3] = {"Piedra", "Papel", "Tijera"};
     while(ronda <= 5){
         printf("\nThis is round number %d", ronda);
         int vuelta = 1;
@@ -19,31 +20,26 @@ int main(int argc, char *argv[]){
             int opcion1;
             scanf("%d", &opcion1);
             int opcion2 = 1 + rand() % 3; 
+            printf("%s -><- %s\n", movimientos[opcion1-1], movimientos[opcion2-1]);
             if(opcion1 == opcion2){
                 printf("Empate\n");
             }
             else if(opcion1 == 1 && opcion2 == 2){
-                printf("Punto para la maquina\n");
                 puntaje_maquina++;
             }
             else if(opcion1 == 2 && opcion2 == 1){
-                printf("Punto para el jugador\n");
                 puntaje_jugador++;
             }
             else if(opcion1 == 1 && opcion2 == 3){
-                printf("Punto para el jugador\n");
                 puntaje_jugador++;
             }
             else if(opcion1 == 3 && opcion2 == 1){
-                printf("Punto para la maquina\n");
                 puntaje_maquina++;
             }
             else if(opcion1 == 2 && opcion2 == 3){
-                printf("Punto para la maquina\n");
                 puntaje_maquina++;
             }
             else if(opcion1 == 3 && opcion2 == 2){
-                printf("Punto para el juagdor\n");
                 puntaje_jugador++;
             }
             vuelta++;
