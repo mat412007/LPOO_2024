6fb79a7a799671214850c7032a9a3540a04d7583
Appaz <matias2007play4@gmail.com>
2024-04-29 10:45:36 -0300

    Update

---
+++ PPP.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char *argv[]){
+    srand(time(NULL));
+    int ronda = 1;
+    int puntaje_jugador = 0;
+    int puntaje_maquina = 0;
+    while(ronda <= 5){
+        printf("\nThis is round number %d\n", ronda);
+        char jugador[10];
+        char maquina[10];
+        printf("What do you choose?\n1- Piedra\n2- Papel\n3- Tijera\n");
+        int opcion1;
+        scanf("%d", &opcion1);
+        int opcion2 = 1 + rand() % 3; 
+        if(opcion1 == opcion2){
+            printf("Empate\n");
+        }
+        else if(opcion1 == 1 && opcion2 == 2){
+            printf("Punto para la maquina\n");
+            puntaje_maquina++;
+        }
+        else if(opcion1 == 2 && opcion2 == 1){
+            printf("Punto para el jugador\n");
+            puntaje_jugador++;
+        }
+        else if(opcion1 == 1 && opcion2 == 3){
+            printf("Punto para el jugador\n");
+            puntaje_jugador++;
+        }
+        else if(opcion1 == 3 && opcion2 == 1){
+            printf("Punto para la maquina\n");
+            puntaje_maquina++;
+        }
+        else if(opcion1 == 2 && opcion2 == 3){
+            printf("Punto para la maquina\n");
+            puntaje_maquina++;
+        }
+        else if(opcion1 == 3 && opcion2 == 2){
+            printf("Punto para el juagdor\n");
+            puntaje_jugador++;
+        }
+        ronda++;
+    }
+    printf("El resultado es:\nJugador -> %d\nMaquina -> %d\n", puntaje_jugador, puntaje_maquina);
+    if(puntaje_jugador < puntaje_maquina){
+        printf("La maquina gana\n");
+    }
+    else if(puntaje_jugador > puntaje_maquina){
+        printf("El jugador gana\n");
+    }
+}
