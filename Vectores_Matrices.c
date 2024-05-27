f4c56fd16c5ac28ba92d273213254545a317c59c
Appaz <matias2007play4@gmail.com>
2024-05-27 17:22:43 -0300

    Vectores y Matrices

--- Vectores_Matrices.c
+++ Vectores_Matrices.c
@@ -33,10 +33,25 @@ int main(int argc, char *argv[]){
             }
             printf("]");
         case 3:
-            printf("\nInserta un numero de cualquier mes\n");
+            printf("\nInserta un numero de mes\n> ");
             int dias_meses[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-            char meses[12] = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
-            
-
+            char *meses[12] = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+            int mes;
+            scanf("%d", &mes);
+            printf("\nEl mes de %s tiene %d dias", meses[mes - 1], dias_meses[mes - 1]);
+        case 4:
+            printf("\nInserta un numero de mes\n> ");
+            int dias_meses2[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+            char *meses2[12] = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+            int mes2;
+            scanf("%d", &mes2);
+            printf("Inserta un numero de dia de mes\n> ");
+            int dia_mes;
+            scanf("%d", &dia_mes);
+            int numero_dia = 0;
+            for(int i = 0; i < mes2; i++){
+                numero_dia += dias_meses2[i];
+            }
+            printf("\nEl dia %d del mes %s es el dia %d",  dia_mes, meses2[mes2 - 1], numero_dia - (dias_meses2[mes2 - 1] - dia_mes));
     }
 }
