cbf231ed8dcb6a97f2f2e374fbb1a571b9b08bfa
Appaz <matias2007play4@gmail.com>
2024-05-30 09:00:17 -0300

    Update

--- Vectores_Matrices.c
+++ Vectores_Matrices.c
@@ -20,6 +20,7 @@ int main(int argc, char *argv[]){
                 printf("%d ", numeros[i]);
             }
             printf("]\nMedia aritmetica = %.2f", promedio/4);
+            break;
         case 2:
             printf("\nInserta 5 numeros\n");
             int numeros2[5];
@@ -32,6 +33,7 @@ int main(int argc, char *argv[]){
                 printf("%d ", numeros2[i]);
             }
             printf("]");
+            break;
         case 3:
             printf("\nInserta un numero de mes\n> ");
             int dias_meses[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
@@ -39,6 +41,7 @@ int main(int argc, char *argv[]){
             int mes;
             scanf("%d", &mes);
             printf("\nEl mes de %s tiene %d dias", meses[mes - 1], dias_meses[mes - 1]);
+            break;
         case 4:
             printf("\nInserta un numero de mes\n> ");
             int dias_meses2[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
@@ -53,6 +56,7 @@ int main(int argc, char *argv[]){
                 numero_dia += dias_meses2[i];
             }
             printf("\nEl dia %d del mes %s es el dia %d",  dia_mes, meses2[mes2 - 1], numero_dia - (dias_meses2[mes2 - 1] - dia_mes));
+            break;
         case 5:
             printf("\nInserta un numero de mes\n> ");
             int dias_meses3[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
@@ -67,6 +71,7 @@ int main(int argc, char *argv[]){
                 fin += dias_meses3[i];
             }       
             printf("\nEl dia %d del mes %s es cuando hay %d dias restantes",  dia_mes2, meses3[mes3 - 1], fin + (dias_meses3[mes3 - 1]) - dia_mes2);   
+            break;
         case 6:
             printf("\nInserta 10 numeros\n");
             int numeros3[10];
@@ -79,6 +84,7 @@ int main(int argc, char *argv[]){
                 printf("%d ", numeros3[i]);
             }
             printf("]");
+            break;
         case 7:
             printf("\nIngresa 10 numeros\n");
             float numeros4[10];
@@ -96,6 +102,7 @@ int main(int argc, char *argv[]){
                 }
             }
             printf("]");
+            break;
         case 8:
             printf("\nIngresa 10 numeros\n");
             int mayor;
@@ -114,6 +121,7 @@ int main(int argc, char *argv[]){
             }
             printf("]\n");
             printf("El mayor es %d", mayor);
+            break;
         case 9:
             printf("\nCalculemos la diferencia de dos vectores\n");
             int vectorA[2];
@@ -127,6 +135,7 @@ int main(int argc, char *argv[]){
             D[1] = vectorA[1] - vectorB[1];
             printf("A = (%d, %d)\nB = (%d, %d)\n", vectorA[0], vectorA[1], vectorB[0], vectorB[1]);
             printf("La diferencia es: D = (%d, %d)", D[0], D[1]);
+            break;
         case 10:
             printf("\nCalculemos el producto escalar de dos vectores\n");
             int vectorX[3];
@@ -139,5 +148,22 @@ int main(int argc, char *argv[]){
             product_esc = vectorX[0]*vectorY[0] + vectorX[1]*vectorY[1] + vectorX[2]*vectorY[2];
             printf("X = (%d, %d, %d)\nY = (%d, %d, %d)\n", vectorX[0], vectorX[1], vectorX[2], vectorY[0], vectorY[1], vectorY[2]);
             printf("El producto escalar es: %d", product_esc);
+            break;
+        case 11:
+            printf("\nCalculemos el producto vectorial de dos vectores\n");
+            int vectorM[3];
+            int vectorN[3];
+            int product_vec[3];
+            printf("Inserta las coordenas del vector M: ");
+            scanf("%d %d %d", &vectorM[0], &vectorM[1], &vectorM[2]);
+            printf("Inserta las coordenas del vector N: ");
+            scanf("%d %d %d", &vectorN[0], &vectorN[1], &vectorN[2]);
+            product_vec[0] = vectorM[1]*vectorN[2] - vectorM[2]*vectorN[1];
+            product_vec[1] = vectorM[2]*vectorN[0] - vectorM[0]*vectorN[2];
+            product_vec[2] = vectorM[0]*vectorN[1] - vectorM[1]*vectorN[0];
+            printf("X = (%d, %d, %d)\nY = (%d, %d, %d)\n", vectorM[0], vectorM[1], vectorM[2], vectorN[0], vectorN[1], vectorN[2]);
+            printf("El producto vectorial es: (%d, %d, %d)", product_vec[0], product_vec[1], product_vec[2]);
+            break;
+        case 12:
     }
 }
