d5dcf4341f93ffc7d705aa3302dfaf587a75b8b2
Appaz <matias2007play4@gmail.com>
2024-06-10 23:14:19 -0300

    Sudoku funcional

--- Sudoku.c
+++ Sudoku.c
@@ -3,29 +3,44 @@
 #include <time.h>
 #include <stdbool.h>
 
-// Imprime el tablero
-void imprimirTablero(int tablero[9][9]) {
-    for(int i = 0; i < 9; i++) {
-        if(i % 3 == 0 && i != 0) {
-            printf("--------------------------------\n");
+// Linea de separacion en el tablero del sudoku
+void linea_horizontal(){
+    for(int x = 0; x < 25; x++){
+        if(x % 8 == 0){
+            printf("|");
         }
+        else{
+            printf("-");
+        }
+    }
+    printf("\n");
+}
+
+// Imprime el tablero ya hecho
+void imprimir_tablero(int tablero[9][9]){
+    linea_horizontal();
+    for(int i = 0; i < 9; i++) {
+        printf("|");
         for(int j = 0; j < 9; j++) {
-            if(j % 3 == 0 && j != 0) {
-                printf(" |");
+            if(tablero[i][j] == 0){
+                printf("  ");
             }
-            if(tablero[i][j] == 0) {
-                printf("   ");
-            } 
             else{
-                printf(" %2d", tablero[i][j]);
+                printf(" %d", tablero[i][j]);   
+            }
+            if((j + 1) % 3 == 0) {
+                printf(" |");
             }
         }
         printf("\n");
+        if((i + 1) % 3 == 0) {
+            linea_horizontal();
+        }
     }
 }
 
 // Verifica si cierto numero es valido para cierta posicion
-bool esValido(int tablero[9][9], int fila, int columna, int numero) {
+bool valido(int tablero[9][9], int fila, int columna, int numero) {
     int inicioFila = fila - fila % 3;
     int inicioColumna = columna - columna % 3;
     for(int i = 0; i < 9; i++) {
@@ -38,14 +53,14 @@ bool esValido(int tablero[9][9], int fila, int columna, int numero) {
 }
 
 // Crea el tablero lleno de numeros
-bool resolverSudoku(int tablero[9][9]) {
+bool resolver(int tablero[9][9]) {
     for(int fila = 0; fila < 9; fila++) {
         for(int columna = 0; columna < 9; columna++) {
             if(tablero[fila][columna] == 0) {
                 for(int numero = 1; numero <= 9; numero++) {
-                    if (esValido(tablero, fila, columna, numero)) {
+                    if (valido(tablero, fila, columna, numero)) {
                         tablero[fila][columna] = numero;
-                        if(resolverSudoku(tablero)) {
+                        if(resolver(tablero)) {
                             return true;
                         }
                         tablero[fila][columna] = 0;
@@ -59,8 +74,7 @@ bool resolverSudoku(int tablero[9][9]) {
 }
 
 // Inserta ceros de manera aleatoria para que el usuario llene sus posiciones
-void backtracking(int tablero[9][9], int numCeros) {
-    srand(time(NULL));
+void espacios_blanco(int tablero[9][9], int numCeros) {
     for(int i = 0; i < numCeros; i++) {
         int fila = rand() % 9;
         int columna = rand() % 9;
@@ -73,33 +87,33 @@ void backtracking(int tablero[9][9], int numCeros) {
 }
 
 // Crea el tablero con espacios para completar
-void crearTablero(int tablero[9][9], int dificultad) {
+void crear_tablero(int tablero[9][9], int dificultad) {
     for(int i = 0; i < 9; i++) {
         for(int j = 0; j < 9; j++) {
             tablero[i][j] = 0;
         }
     }
 
-    resolverSudoku(tablero);
+    resolver(tablero);
 
     switch(dificultad) {
         case 1: 
-            backtracking(tablero, 18 + rand() % 10); 
+            espacios_blanco(tablero, 15 + rand() % 10); 
             break;
         case 2: 
-            backtracking(tablero, 30 + rand() % 10); 
+            espacios_blanco(tablero, 30 + rand() % 10); 
             break;
         case 3: 
-            backtracking(tablero, 40 + rand() % 10); 
+            espacios_blanco(tablero, 42 + rand() % 10); 
             break;
         default: 
-            backtracking(tablero, 30 + rand() % 10); 
+            espacios_blanco(tablero, 30 + rand() % 10); 
             break;
     }
 }
 
 // Verifica si el tablero esta completo y el juego ha acabado
-bool tableroCompleto(int tablero[9][9]) {
+bool tablero_completado(int tablero[9][9]) {
     for(int i = 0; i < 9; i++) {
         for(int j = 0; j < 9; j++) {
             if(tablero[i][j] == 0) {
@@ -110,19 +124,32 @@ bool tableroCompleto(int tablero[9][9]) {
     return true;
 }
 
+// Verifica si el espacio para completar ya ha sido completado
+bool lleno(int tablero[9][9], int fila, int columna){
+    for(int i = 0; i < 9; i++){
+        for(int j = 0; j < 9; j++){
+            if(tablero[i][j] == tablero[fila][columna]){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
 int main(int argc, char *argv[]){
+    srand(time(NULL));
     int tablero[9][9] = {0};
     printf("Seleccione la dificultad:\n1. Facil\n2. Normal\n3. Dificil\n> ");
     int dificultad;
     scanf("%d", &dificultad);
-    crearTablero(tablero, dificultad);
+    crear_tablero(tablero, dificultad);
     printf("Tablero inicial:\n");
-    imprimirTablero(tablero);
+    imprimir_tablero(tablero);
 
     int intentos = 3;
     int fila, columna, numero;
 
-    while(tableroCompleto(tablero) == 0 && intentos > 0) {
+    while(tablero_completado(tablero) == 0 && intentos > 0) {
         printf("Ingrese fila: ");
         scanf("%d", &fila);
         printf("Ingrese columna: ");
@@ -139,11 +166,15 @@ int main(int argc, char *argv[]){
         fila--; 
         columna--;
 
-        if(tablero[fila][columna] == 0 && esValido(tablero, fila, columna, numero)) {
+        if(tablero[fila][columna] == 0 && valido(tablero, fila, columna, numero)) {
             tablero[fila][columna] = numero;
             printf("\nMovimiento valido. \nTablero actual:\n");
-            imprimirTablero(tablero);
+            imprimir_tablero(tablero);
         } 
+        else if(lleno(tablero, fila, columna)){
+            printf("\nEse espacio ya ha sido completado.\nIntentalo de nuevo\n");
+            intentos--;
+        }
         else{
             printf("\nMovimiento invalido. \nIntente de nuevo.\n");
             intentos--;
@@ -155,7 +186,7 @@ int main(int argc, char *argv[]){
         }
     }
 
-    if(tableroCompleto(tablero)) {
+    if(tablero_completado(tablero)) {
         printf("\n¡Felicidades, completaste el Sudoku!\n");
     }
 }
