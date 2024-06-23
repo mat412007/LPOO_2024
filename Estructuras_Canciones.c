6e375d59e871c31e68a999861198da4a310e3129
Appaz <matias2007play4@gmail.com>
2024-06-23 00:52:02 -0300

    Update

--- Estructuras_Canciones.c
+++ Estructuras_Canciones.c
@@ -13,7 +13,10 @@ int menu_opciones(){
     int eleccion;
     printf("\n1- Nueva cancion\n");
     printf("2- Mostrar titulos de canciones\n");
-    printf("3- Buscar cancion\n> ");
+    printf("3- Buscar cancion\n");
+    printf("4- Mostrar todas las canciones\n");
+    printf("5- Borrar una cancion\n");
+    printf("6- Modificar una cancion\n> ");
     scanf("%d", &eleccion);
     printf("\n");
     return eleccion;
@@ -21,15 +24,21 @@ int menu_opciones(){
 
 void accion_1(struct cancion canciones[100], int numero_cancion){
     struct cancion cancion_nueva;
-    printf("Artista de la cancion: ");
-    scanf("%s", &cancion_nueva.artista);
-    printf("Titulo de la cancion: ");
-    scanf("%s", &cancion_nueva.titulo);
-    printf("Cuanto dura la cancion(segundos): ");
-    scanf("%d", &cancion_nueva.duracion_segundos);
-    printf("Size de la cancion(KB): ");
-    scanf("%d", &cancion_nueva.size_KB);
-    canciones[numero_cancion] = cancion_nueva;
+    while(1){
+        printf("Artista de la cancion: ");
+        scanf("%s", &cancion_nueva.artista);
+        printf("Titulo de la cancion: ");
+        scanf("%s", &cancion_nueva.titulo);
+        printf("Cuanto dura la cancion(segundos): ");
+        scanf("%d", &cancion_nueva.duracion_segundos);
+        if(cancion_nueva.duracion_segundos < 0){ printf("Error. La duracion no puede ser negativa\n\n"); continue; }
+        printf("Size de la cancion(KB): ");
+        scanf("%d", &cancion_nueva.size_KB);
+        if(cancion_nueva.size_KB < 0){ printf("Error. El size no puede ser negativo\n\n"); continue; } 
+        canciones[numero_cancion] = cancion_nueva;
+        break;
+    }
+
 }
 
 void imprimir_cancion(struct cancion canciones[100], int elemento){
@@ -67,10 +76,59 @@ int main(int argc, char *argv[]){
             scanf("%s", &artista_titulo);
             printf("Canciones encontradas: \n");
             for(int i = 0; i < numero_cancion; i++){
+                // strstr retorna null si el texto introducido no esta presente en la variable en comparacion
                 if(strstr(canciones[i].artista, artista_titulo) != NULL || strstr(canciones[i].titulo, artista_titulo) != NULL){
                     imprimir_cancion(canciones, i);
                 }
             }
+        }
+        else if(accion == 4){
+            for(int i = 0; i < numero_cancion; i++){
+                imprimir_cancion(canciones, i);
+            }
+        }
+        else if(accion == 5){
+            int indice_borrar;
+            printf("Inserta el numero de la cancion de la lista que deseas borrar, no el indice: ");
+            scanf("%d", &indice_borrar);
+            for(int i = indice_borrar-1; i < numero_cancion; i++){
+                canciones[i] = canciones[i+1];
+            }
+            numero_cancion--;
+        }
+        else if(accion == 6){
+            int indice_modificar;
+            printf("Inserta el numero de la cancion de la lista que deseas modificar, no el indice: ");
+            scanf("%d", &indice_modificar);
+            indice_modificar--;
+            int modificacion;
+            printf("Que deseas modificar:\n1- Artista\n2- Titulo\n3- Duracion\n4- Size\n> ");
+            scanf("%d", & modificacion);
+            while(1){
+                if(modificacion == 1){
+                    printf("Artista de la cancion: ");
+                    scanf("%s", &canciones[indice_modificar].artista);
+                }
+                else if(modificacion == 2){
+                    printf("Titulo de la cancion: ");
+                    scanf("%s", &canciones[indice_modificar].titulo);
+                }
+                else if(modificacion == 3){
+                    printf("Cuanto dura la cancion(segundos): ");
+                    scanf("%d", &canciones[indice_modificar].duracion_segundos);
+                    if(canciones[indice_modificar].duracion_segundos < 0){ printf("Error. La duracion no puede ser negativa\n\n"); continue; }
+                }
+                else if(modificacion == 4){
+                    printf("Size de la cancion(KB): ");
+                    scanf("%d", &canciones[indice_modificar].size_KB);
+                    if(canciones[indice_modificar].size_KB < 0){ printf("Error. El size no puede ser negativa\n\n"); continue; }
+                }
+                break;
+            }
+        }
+        else{
+            printf("\nOpcion invalida, ingresa una opcion del menu de opciones\n");
+            continue;
         }
     }
 }
