b9fc2aa81f00cf38cceb71d9bf9bef2cc54810d9
Appaz <matias2007play4@gmail.com>
2024-07-08 23:53:07 -0300

    Punteros

---
+++ Punteros_Referencias.c
@@ -0,0 +1,196 @@
+#include <stdio.h>
+
+void ingresar_numeros(int *numeros) {
+    for(int i = 0; i < 5; i++) {
+        printf("Ingresa el valor numero %d: ", i + 1);
+        scanf("%d", &numeros[i]);
+    }
+}
+
+void ordenar_numeros(int *numeros){
+    for(int i = 0; i < 4; i++){
+        int menor = numeros[i];
+        int indice;
+        for(int j = i; j < 5; j++){
+            if(numeros[j] <= menor){
+                menor = numeros[j];
+                indice = j;
+            }
+        }
+        numeros[indice] = numeros[i];
+        numeros[i] = menor;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    int ejercicio;
+    printf("Que ejercicio deseas corregir?\n> ");
+    scanf("%d", &ejercicio);
+    switch(ejercicio) {
+        case 1: {
+            int a, b, c, d, e;
+            int numeros[5];
+            ingresar_numeros(numeros);
+            ordenar_numeros(numeros);
+            a = numeros[0];
+            b = numeros[1];
+            c = numeros[2];
+            d = numeros[3];
+            e = numeros[4];
+            printf("\na = %d", a);
+            printf("\nb = %d", b);
+            printf("\nc = %d", c);
+            printf("\nd = %d", d);
+            printf("\ne = %d", e);
+            break;
+        }
+    }
+}
+
+
+/* #include <stdio.h>
+int main() {
+    int x, *y; // Definimos un entero y un puntero
+    x=10; // Valor del entero
+    y=&x; // Direccion del puntero
+    printf("%d %d %d",y,*y, &y); // El primero y tercero están mal, ya que estos imprimen direcciones de la memoria, el del medio imprime el valor guardado en la sección de memoria que el puntero apunta 
+	return 0;
+} */
+
+
+
+/* #include <stdio.h>
+void carga (int n, int Vector[]); // Declaracion de funciones del programa
+void imprimir (int n, int Vector[]);
+ 
+int main(){
+    int n=5; // Inicio un entero con 5 de valor
+    int V[n]; // Inicio una matriz de enteros de n elementos
+    carga (n,V); // Ingresamos los numeros a la matriz V
+    imprimir (n,V); // Imprimimos los elementos de la matriz V verticalmente
+    printf("\n");
+    for (int i=0; i<n; i++){ // Imprimimos los elementos de la matriz V horizontalmente
+        printf("%5d",V[i]);
+    }
+    return 0;
+}
+
+void carga (int n, int Vector[]){ // Funcion para llenar la matriz
+    int i;
+    for (i=0; i<n; i++){
+        printf("ingrese el valor de la posicion %d: ",i);
+        scanf("%d",&Vector[i]);
+     }
+}
+void imprimir (int n, int Vector[]){ // Funcion para imprimir la matriz
+    for (int i=0; i<n; i++){
+        printf("\n %5d",Vector[i]);
+	}
+    printf("\n");
+} */
+
+
+
+/* #include <stdio.h>
+void carga (int n, int Vector[]); // Declaracion de funciones del programa
+void imprimir (int n, int Vector[]);
+void ordenar (int n, int Vector[]);
+
+int main(){
+    int n=5; // Inicio un entero con 5 de valor
+    int V[n]; // Inicio una matriz de tamaño n
+    carga (n,V); // Ingresamos los elementos de V
+    imprimir (n,V); // Imprimimos los elementos de V en el mismo orden que fueron ingresados
+    ordenar (n,V);
+    imprimir (n,V); // Imprimimos la matriz V con su nuevo orden verticalmente
+    printf("\n");
+    for (int i=0; i<n; i++){ // Imprimimos la matriz V con su nuevo orden horizontalmente
+        printf("%5d",V[i]);
+	}
+	return 0;
+}
+
+void carga (int n, int Vector[]){ // Funcion para insertar elementos en la matriz
+    for (int i=0; i<n; i++){
+        printf("ingrese el valor de la posicion %d: ",i);
+        scanf("%d",&Vector[i]);
+    }
+}
+void imprimir (int n, int Vector[]){ // Funcion para imprimir la matriz verticalmente
+    for (int i=0; i<n; i++){
+        printf("\n %5d",Vector[i]);
+	}
+    printf("\n");
+}
+void ordenar (int n, int Vector[]){ // Funcion de intercambio y orden de valores
+    int aux;
+    for (int i=0; i<n; i++){
+        for (int j=0; j<n; j++){
+            if (Vector[i]>Vector[j]){ // Intercambia ambos valores de posicion
+                aux=Vector[i];
+                Vector[i]=Vector[j];
+                Vector[j]=aux;
+            }
+ 	    }
+    }
+    printf("\n");
+} */
+
+
+/* 
+#define N 10
+#include <stdio.h>
+void leer_matriz(int A[N][N], int filas, int columnas); // Declaraciones de funciones del programa
+void escribir_matriz(int A[N][N], int filas, int columnas);
+void multiplicar(int A[N][N], int B[N][N], int C[N][N], int m, int n, int p);
+
+void leer_matriz(int A[N][N], int filas, int columnas) { // Funcion para insertar los elementos de la matriz
+    int i, j;
+    for (i=0; i<filas; i++)  {
+        for (j=0; j<columnas; j++)  {
+            printf("Ingresa la entrada [%d][%d]: ", i+1, j+1);
+            scanf("%d", &A[i][j]);
+        }
+    }
+}
+void escribir_matriz(int A[N][N], int filas, int columnas){ // Funcion para imprimir las matrices
+    int i, j;
+    for (i=0; i< filas; i++)  {
+        for (j=0; j<columnas; j++)   {
+            printf("\t%d", A[i][j]);
+        }
+    printf("\n");
+    }
+}
+void multiplicar (int A[N][N], int B[N][N], int C[N][N], int m, int n, int p) { // Funcion que multiplica 2 matrices cuando el largo o ancho de una es igual al largo o ancho de la otra
+    int i, j, k;
+    for(i=0; i< m; i++)
+        for(k=0; k< p; k++)
+            C[i][k]=0;
+            for (i=0; i< m; i++)
+                for (j=0; j< n; j++)
+                    for (k=0; k< p; k++)
+                        C[i][k] = C[i][k] + A[i][j]*B[j][k];
+}
+int main ()  {
+    int m, n, p; // Declaro tres variables enteras
+    int A[N][N], B[N][N], C[N][N]; // Declaro tres matrices del mismo tamaño
+    printf("\n Datos de la Matriz A: \n"); 
+    printf("Filas: ");                           	
+    scanf("%d", &m);
+    printf("Columnas: "); 	 // Lleno de valores las primeras dos matrices
+    scanf("%d", &n);
+    leer_matriz(A, m, n);
+    printf("\n Datos de la Matriz B: \n");
+    printf("Columnas: ");	            	
+    scanf("%d", &p);
+    leer_matriz(B, n, p);
+    printf("\n Matriz A:\n");  
+    escribir_matriz(A, m, n);
+    printf("\n Matriz B: \n");  
+    escribir_matriz(B, n, p);
+    multiplicar(A, B, C, m, n, p); // Lleno la tercera matriz con los valores de la multiplicacion de las 2 primeras
+    printf("\n Matriz AxB: \n");   
+    escribir_matriz(C, m, p);
+}
+ */
