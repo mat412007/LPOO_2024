da35889bf955cd90981dcb3bc609f651cba2acfd
Appaz <matias2007play4@gmail.com>
2024-08-12 22:56:21 -0300

    Correcciones

--- Java/Primeros_Ejercicios.java
+++ Java/Primeros_Ejercicios.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
-public class Primary_Class {
+// javac -target 8 -source 8 file.java / javac --release 8 introduction.java
+
+public class Primeros_Ejercicios {
 
     static boolean primo(int numero) {
 		for(int i = numero-1; i != 1; i--) {
@@ -12,10 +14,10 @@ public class Primary_Class {
 	}
 
 	public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.println("Que ejercicio deseas corregir?: ");
-		int ejercicio = sc.nextInt();
-
+		int ejercicio = input.nextInt();
+        System.out.println("");
         switch(ejercicio){
             case 1:
                 for(int i = 1; i <= 100; i++) {
@@ -26,6 +28,7 @@ public class Primary_Class {
 		        		System.out.println(i);
 		        	}
 		        }
+                break;
             case 2:
                 for(int i = 50; i <= 100; i++) {
 			
@@ -37,29 +40,35 @@ public class Primary_Class {
 			        }
 			
 		        }
+                break;
             case 3:
                 while(true) {
                     System.out.println("Ingresa un numero");
-                    int numero = sc.nextInt();
+                    int numero = input.nextInt();
                     if((primo(numero) == true) && numero > 100){
                         System.out.println("Has ingresado un numero primo mayor a 100\nEs el fin");
                         break;
                     }
                     System.out.println("");
                 }
+                break;
             case 4:
                 System.out.println("5 + 5 = " + (5+5));
                 System.out.println("10 - 2 = " + (10-2));
                 System.out.println("10 * 10 = " + (10*10));
                 System.out.println("25 / 5 = " + (25/5));
+                break;
             case 5:
-                String armas = {"Espada", "Escudo", "Lanza", "Arco", "Flecha", "Hacha"};
+                String[] armas = {"Espada", "Escudo", "Lanza", "Arco", "Flecha", "Hacha"};
                 int num_arma = 1;
-                System.out.println("Cuales son armas de guerra antiguas?");
+                System.out.println("Armas de guerra antiguas:");
                 for(String a: armas){
                     System.out.println(num_arma + "- " + a);
                     num_arma++;
                 }
+                break;
+            default:
+                System.out.println("Ingresaste un numero de ejercicio fuera de rango\nLas opciones son de 1 a 5\nIntentelo de nuevo\n");
         }
 	}
 
